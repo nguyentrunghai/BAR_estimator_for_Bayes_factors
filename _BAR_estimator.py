d9e8f87c5116c416866f097e2f5d5b778da529f4
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-03-26 11:23:51 +0700

    add new file

---
+++ _BAR_estimator.py
@@ -0,0 +1,34 @@
+"""
+Functions the implement BAR estimator
+"""
+
+
+def dict_to_list(dict_of_list):
+    """
+    :param dict_of_list: dict: varname --> ndarray
+    :return: list of dic: [ {varname: float, ...}, ...  ]
+    """
+    keys = list(dict_of_list.keys())
+    key0 = keys[0]
+    for key in keys[1:]:
+        assert len(dict_of_list[key0]) == len(dict_of_list[key]), key0 + " and " + key + " do not have same len."
+
+    n = len(dict_of_list[key0])
+    ls_of_dic = []
+    for i in range(n):
+        dic = {key: dict_of_list[key][i] for key in keys}
+        ls_of_dic.append(dic)
+    return ls_of_dic
+
+
+def get_values_from_trace(model, trace, burn=0):
+    """
+    :param model: pymc3 model
+    :param trace: pymc3 trace object
+    :param burn: int, number of steps to exclude
+    :return: dict: varname --> ndarray
+    """
+    varnames = [var.name for var in model.vars]
+    trace_values = {var: trace.get_values(var, burn=burn) for var in varnames}
+    return trace_values
+
